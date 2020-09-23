f12dc236d65b2624a868b1e784677aaedc234bd8
Jeroen Claassens <support@favware.tech>
2020-09-23 22:50:18 +0200

    feat: add with-node-canvas image

--- with-node-canvas/control.ps1
+++ with-node-canvas/control.ps1
@@ -5,9 +5,8 @@ Function Step-with-node-canvas {
 
     Process {
         switch ( $Command ) {
-            build { docker build -t favware/codespaces-containers:with-node-canvas -t favware/codespaces-containers:with-canvas -t favware/codespaces-containers:canvas . }
-            deploy { docker push favware/codespaces-containers:with-node-canvas && docker push favware/codespaces-containers:with-canvas && docker push favware/codespaces-containers:canvas }
-            remove { docker rmi -f favware/codespaces-containers }
+            build { docker build -t favware/codespaces-containers:canvas . }
+            remove { docker rmi -f favware/codespaces-containers:canvas }
             default { Write-Host "Unrecognized command, please try again" -ForegroundColor Red }
         }
     }
